5f6872ffb07e1db22e2963eaf7616c338a58a2f5
Daniel McIntosh <dmcintosh@exitsoft.co.uk>
2024-08-26 17:12:08 +0100

    Started 06) Ticket Management

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,15 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+
+    let mut fib_nums: Vec<u32> = vec![0, 1, ];
+    let n = n as usize;
+
+    for i in 2..=n {
+        fib_nums.push(fib_nums[i - 2] + fib_nums[i - 1]);
+    }
+
+    fib_nums[n]
 }
 
 #[cfg(test)]
