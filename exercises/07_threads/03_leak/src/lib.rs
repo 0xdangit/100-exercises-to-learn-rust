14fb68e2c172a20b2c4916c6ebe462029c238e6c
Daniel McIntosh <dmcintosh@exitsoft.co.uk>
2024-09-12 13:07:28 +0100

    Continued working on 07) Threads

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,15 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let leaked_v = v.leak();
+
+    let (v1, v2) = leaked_v.split_at(leaked_v.len() / 2);
+
+    let h1 = thread::spawn(|| v1.iter().sum::<i32>());
+    let h2 = thread::spawn(|| v2.iter().sum::<i32>());
+
+    h1.join().unwrap() + h2.join().unwrap()
+
 }
 
 #[cfg(test)]
